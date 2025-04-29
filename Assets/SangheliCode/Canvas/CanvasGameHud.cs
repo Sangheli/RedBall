09240ad81919e5ceebe354ff85f87af5ddc5d24c
Sangheli <a.savel.vic@gmail.com>
2025-04-30 01:52:19 +0400

    show target text

--- Assets/SangheliCode/Canvas/CanvasGameHud.cs
+++ Assets/SangheliCode/Canvas/CanvasGameHud.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 public class CanvasGameHud : MonoBehaviour
 {
     [SerializeField] private TMP_Text textCollectibleCount;
+    [SerializeField] private TMP_Text targetText;
     
     public void Init()
     {
         G.Instance.OnCollectableUpdate += OnCollectibleFound;
+        targetText.text = $"{G.Instance.gameMode.WinCollectableValue}";
         OnCollectibleFound();
     }
 
